edb11a088c0473b39a9ca774e8eae7de91a4f6d3
Kayo Ronald <kayoronald4321@gmail.com>
2023-04-08 11:56:36 +0000

    fix: deno logger

--- app/config/logger.ts
+++ app/config/logger.ts
@@ -1,22 +1,31 @@
-import { log } from '../deps.ts';
+import * as log from "https://deno.land/std/log/mod.ts";
 await log.setup({
   //define handlers
   handlers: {
     console: new log.handlers.ConsoleHandler("DEBUG", {
-      formatter: "[{levelName}] {msg}",
+      formatter: "[{levelName}] {msg}"
     }),
+    // file: new log.handlers.RotatingFileHandler('INFO', {
+    //   filename: './a.log',
+    //   maxBytes: 15,
+    //   maxBackupCount: 5,
+    //   formatter: rec => JSON.stringify({ region: rec.loggerName, ts: rec.datetime, level: rec.levelName, data: rec.msg })
+    // })
   },
+
   //assign handlers to loggers  
   loggers: {
     default: {
       level: "DEBUG",
+      handlers: ["console"],
     },
     client: {
       level: "INFO",
-    },
+      handlers: ["file"]
+    }
   },
 });
-
 const logger = log.getLogger();
+// const cl = log.getLogger('client');
 
 export default logger
